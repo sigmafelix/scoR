27bb80ff63ef8175a8d80d85069294ea4f7c8a01
sigmafelix <sigmafelix@gmail.com>
2017-02-22 00:13:47 +0900

    Create Poly_proportion_buffer.R

---
+++ Poly_proportion_buffer.R
@@ -0,0 +1,30 @@
+C_poly_proportion_buffer = function(points, target_sp, width=500, id_field=NULL){
+  # You should project your spatial dataset before execution.
+  # sp = intersected
+  # poly.buf = buffer features
+  library(rgeos)
+  library(sp)
+  library(rgdal)
+  library(dplyr)
+  
+  # Buffer generation
+  buffer = gBuffer(points, byid = TRUE, 
+                   width = width, quadsegs=90)
+  sp = gIntersection(buffer, target_sp, byid = TRUE)
+  
+  sp.id = c()
+  buf.area = buffer@polygons[[1]]@area
+  for (i in 1:length(sp)){ sp.id[i] = sp@polygons[[i]]@ID}
+  sp.id = sapply(strsplit(sp.id, split = ' '),  function(x) x[1])
+  sp.area = sapply(sp@polygons, function(x) x@area)
+  poly.df = data.frame(ID=sp.id, Shp_area=sp.area) %>% 
+    group_by(ID) %>% 
+    summarize(sp.area = sum(Shp_area)) %>% 
+    ungroup() %>% 
+    mutate(ID = as.integer(as.character(ID)) + 1,
+           sp.area = sp.area / buf.area) %>% 
+    arrange(ID) %>% 
+    mutate(amsid = points@data[ID, id_field]) %>% 
+    dplyr::select(3,2)
+  return(poly.df)
+  }
